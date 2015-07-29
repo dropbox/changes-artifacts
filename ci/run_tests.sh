1889f9f6e05fb1d59d1ceea3c7dc4821fe63ae56
Anup Chenthamarakshan <anupc@dropbox.com>
2015-07-29 11:33:16 -0700

    Export Junit XML.

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -5,4 +5,8 @@ ls -lh
 
 cd $GOPATH/src/github.com/dropbox/changes-artifacts/
 go get -v ./...
-go test -v -short ./...
+
+go get -v github.com/jstemmer/go-junit-report
+
+go test -short -v ./... | tee test.output | go-junit-report > junit.xml
+cat test.output
