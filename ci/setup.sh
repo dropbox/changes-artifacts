1c3289f397e656d6a8d2c9c5d72a11019c6e3834
Anup Chenthamarakshan <anupc@dropbox.com>
2015-07-29 09:56:04 -0700

    Only install Go binary if it is not already installed.

--- ci/setup.sh
+++ ci/setup.sh
@@ -2,6 +2,15 @@
 
 GO_VERSION=1.4
 
-cd /tmp
-wget "http://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz"
-sudo tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
+if [ ! -x /usr/local/go/bin/go ]
+then
+  echo "Installing Go binary...."
+  cd /tmp
+  wget "http://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz"
+  sudo tar -C /usr/local -xzf "go${GO_VERSION}.linux-amd64.tar.gz"
+  echo "Installed Go binary...."
+else
+  echo "Go binary already installed"
+fi
+
+/usr/local/go/bin/go version
