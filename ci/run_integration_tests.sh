2afaae63bf87bc9603f590df8697dc0f2983f597
Anup Chenthamarakshan <anupc@dropbox.com>
2015-08-26 08:46:11 -0700

    Return correct exit code using go test status while using go-junit-report.
    Summary: Test script should not return successful exit code if `go test` failed.
    
    Test Plan: Tested with failing `go test`
    
    Reviewers: kylec
    
    Reviewed By: kylec
    
    Subscribers: changesbot, anupc
    
    Differential Revision: https://tails.corp.dropbox.com/D133524

--- ci/run_integration_tests.sh
+++ ci/run_integration_tests.sh
@@ -13,7 +13,11 @@ sudo fakes3 -r /var/cache/fakes3 -p 4569 &
 
 go test -race -cover -v ./... | tee test.output | go-junit-report > junit.xml
 
+statuscode=${PIPESTATUS[0]}
+
 pkill -9 server
 sudo pkill -9 fakes3
 
 cat test.output
+
+exit $statuscode
